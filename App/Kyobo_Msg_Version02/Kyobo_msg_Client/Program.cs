8986a2d6dc17f56973546276bb0195e70a6b5e26
moskito78 <kenshukimu@gmail.com>
2021-02-22 17:40:38 +0900

    프로세스 체크로직 추가

--- App/Kyobo_Msg_Version02/Kyobo_msg_Client/Program.cs
+++ App/Kyobo_Msg_Version02/Kyobo_msg_Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,9 +18,45 @@ namespace Kyobo_Msg_Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            MainProg.Instance.Init();
-            
-            Application.Run(MainProg.GetLoginForm());
+            // Get Reference to the current Process
+            Process thisProc = Process.GetCurrentProcess();
+
+            if (IsProcessOpen("Kyobo_Msg_Client") == false)
+            {
+                //System.Windows.MessageBox.Show("Application not open!");
+                //System.Windows.Application.Current.Shutdown();
+                MainProg.Instance.Init();
+                Application.Run(MainProg.GetLoginForm());
+            }
+            else
+            {
+                // Check how many total processes have the same name as the current one
+                if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+                {
+                    // If ther is more than one, than it is already running.
+                    System.Windows.MessageBox.Show("프로그램이 실행 중에 있습니다.");
+                    Application.Exit();
+                    return;
+                }
+                else
+                {
+                    MainProg.Instance.Init();
+                    Application.Run(MainProg.GetLoginForm());
+                }
+            }
+        }
+
+        public static bool IsProcessOpen(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
